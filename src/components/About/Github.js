e2677e4487db852fb4144e0a76b0884087705c23
Param Choudhary <choudharyparam99@gmail.com>
2021-09-19 17:01:24 +0530

    my first portfolio website

---
+++ src/components/About/Github.js
@@ -0,0 +1,31 @@
+import React from "react";
+import GitHubCalendar from "react-github-calendar";
+import { Row } from "react-bootstrap";
+
+function Github() {
+  const colourTheme = {
+    background: "transparent",
+    text: "#f4f4f4",
+    grade4: "#8400b8",
+    grade3: "#b22ff4",
+    grade2: "#b265f6",
+    grade1: "#c084f2",
+    grade0: "#f4f4f4",
+  };
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+      <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
+        Days I <strong className="purple">Code</strong>
+      </h1>
+      <GitHubCalendar
+        username="paramchoudhary"
+        blockSize={15}
+        blockMargin={5}
+        theme={colourTheme}
+        fontSize={16}
+      />
+    </Row>
+  );
+}
+
+export default Github;
